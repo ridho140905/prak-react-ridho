2295384f1ed0711a422f64ed4397356c1949d218
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-03-31 16:25:01 +0700

    reusable component, reactfrom state ,conditiotal rendrering

--- src/pertemuan-3/main.jsx
+++ src/pertemuan-3/main.jsx
@@ -1,10 +1,13 @@
 import { createRoot } from "react-dom/client";
 import TailwindCSS from "./TailwindCSS";
-import './tailwind.css';
-createRoot(document.getElementById("root"))
-    .render(
-        <div>
-            <TailwindCSS/>
-        </div>
-    )
+import "./tailwind.css";
+import UserForm from "./UserForm";
+import HitungGajiForm from "./HitungGajiForm"
 
+createRoot(document.getElementById("root")).render(
+  <div>
+    {/* <TailwindCSS /> */}
+    <UserForm />
+    <HitungGajiForm/>
+  </div>,
+);
