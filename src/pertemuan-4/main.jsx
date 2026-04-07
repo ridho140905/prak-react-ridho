01198ecb5953a3deac0c1e55577b8662951c4069
ridho140905 <ridho24si@mahasiswa.pcr.ac.id>
2026-04-07 16:31:53 +0700

    penambahan responsive & grid design

--- src/pertemuan-4/main.jsx
+++ src/pertemuan-4/main.jsx
@@ -3,11 +3,14 @@ import TailwindCSS from "./TailwindCSS";
 import "./tailwind.css";
 import FrameworkList from "./FrameworkList";
 import FrameworkListSearchFilter from "./FrameworkListSearchFilter";
+import ResponsiveDesign from "./ResponsiveDesign";
+
 
 createRoot(document.getElementById("root")).render(
   <div>
     {/* <TailwindCSS /> */}
     {/* <FrameworkList /> */}
-    <FrameworkListSearchFilter />
+    {/* <FrameworkListSearchFilter /> */}
+    <ResponsiveDesign />
   </div>,
 );
